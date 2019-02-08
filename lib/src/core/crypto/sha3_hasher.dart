4fd607c295d0b5c88b9633757d0533cfe5560009
Fajar van Megen <3041538+fajarvm@users.noreply.github.com>
2019-02-08 15:57:15 +0100

    Added some functions to the Sha3Hasher class to be able to reset, update and finalize the hash.

--- lib/src/core/crypto/sha3_hasher.dart
+++ lib/src/core/crypto/sha3_hasher.dart
@@ -3,20 +3,54 @@ library nem2_sdk_dart.core.crypto.sha3_hasher;
 import 'dart:typed_data' show Uint8List;
 
 import 'sha3nist.dart';
+import '../utils/hex_utils.dart';
+import '../utils/array_utils.dart';
 
 class Sha3Hasher {
+  final SHA3DigestNist _hasher;
+
+  Sha3Hasher._(this._hasher);
+
+//  factory Sha3Hasher({final int length = 64}) {
+//    return createHasher(length: length);
+//  }
+
   /// Calculate hash of the [data] and return the hash as result.
   /// The optional parameter [length] defines the hash length in bytes. It
   /// accepts an [int] value of 32 or 64. By default, the value is set to 64.
   static Uint8List hash(Uint8List data, {final int length = 64}) {
-    final SHA3DigestNist digest = getHasher(length: length);
+    final SHA3DigestNist digest = _getHasher(length: length);
     digest.reset();
     return digest.process(data);
   }
 
+  static Sha3Hasher createHasher({final int length = 64}) {
+    return Sha3Hasher._(_getHasher(length: length));
+  }
+
+  void reset() {
+    this._hasher.reset();
+  }
+
+  void update(var data) {
+    if (data is String) {
+      final Uint8List byte = HexUtils.getBytes(data);
+      this._hasher.update(byte, 0, 8);
+    } else if (data is Uint8List) {
+      this._hasher.update(data, 0, 8);
+    } else {
+      throw new ArgumentError('unsupported data type');
+    }
+  }
+
+  void finalize(Uint8List result) {
+    result = this._hasher.process(result);
+//    ArrayUtils.copy(result, this._hasher.process(result));
+  }
+
   // ------------------------------ private / protected functions ------------------------------ //
 
-  static SHA3DigestNist getHasher({final int length = 64}) {
+  static SHA3DigestNist _getHasher({final int length = 64}) {
     if (length == 64) {
       return new SHA3DigestNist(512);
     }
--- test/core/crypto/sha3_hasher_test.dart
+++ test/core/crypto/sha3_hasher_test.dart
@@ -3,7 +3,7 @@ library nem2_sdk_dart.test.core.crypto.sha3_hasher_test;
 import "package:test/test.dart";
 import "dart:typed_data" show Uint8List;
 
-import "package:nem2_sdk_dart/src/core/crypto.dart" show Sha3Hasher;
+import "package:nem2_sdk_dart/src/core/crypto.dart" show Sha3Hasher, SHA3DigestNist;
 import "package:nem2_sdk_dart/src/core/utils.dart" show HexUtils;
 
 main() {
@@ -16,7 +16,9 @@ main() {
     '9F2FCC7C90DE090D6B87CD7E9718C1EA6CB21118FC2D5DE9F97E5DB6AC1E9C10'
   ];
 
-  group('test hash function', () {
+  group('Test hasher (SHA3 256)', () {
+    // Test vectors are taken from :
+    // https://github.com/gvanas/KeccakCodePackage/blob/master/TestVectors/ShortMsgKAT_SHA3-256.txt
     const List<String> expectedOutput = [
       'A7FFC6F8BF1ED76651C14756A061D662F580FF4DE43B49FA82D80A4B80F8434A',
       '677035391CD3701293D385F037BA32796252BB7CE180B00B582DD9B20AAAD7F0',
@@ -26,16 +28,16 @@ main() {
       '2F1A5F7159E34EA19CDDC70EBF9B81F1A66DB40615D7EAD3CC1F1B954D82A3AF'
     ];
 
-    test('Can hash test vectors (SHA3 256)', () {
-      final int length = 32; // SHA3-256
+    final int length = 32; // SHA3-256
 
+    test('SHA3 256 can hash test vectors', () {
       // sanity check
       expect(expectedOutput.length, equals(inputs.length));
 
       for (int i = 0; i < inputs.length; i++) {
-        final String inputHex = inputs[3];
+        final String inputHex = inputs[i];
         final Uint8List inputBuffer = HexUtils.getBytes(inputHex);
-        final expectedHash = expectedOutput[3];
+        final expectedHash = expectedOutput[i];
 
         Uint8List hash = Sha3Hasher.hash(inputBuffer, length: length);
 
@@ -44,6 +46,59 @@ main() {
       }
     });
 
-    /// TODO: complete unit test
+//    test('SHA3 256 can hash test vectors in two parts', () {
+//      // sanity check
+//      expect(expectedOutput.length, equals(inputs.length));
+//
+////      for (int i = 0; i < inputs.length; i++) {
+//        final String inputHex = inputs[0];
+//        final Uint8List inputBuffer = HexUtils.getBytes(inputHex);
+//        final expectedHash = expectedOutput[0];
+//
+//        final Sha3Hasher hasher = Sha3Hasher.createHasher(length: length);
+//        hasher.reset();
+//
+//        // hash the input in two parts
+//        hasher.update(inputBuffer.sublist(0, inputBuffer.lengthInBytes ~/ 2));
+//        hasher.update(inputBuffer.sublist(inputBuffer.lengthInBytes ~/ 2));
+//
+//        Uint8List hash = new Uint8List(length);
+//        hasher.finalize(hash);
+//
+//        final String hashString = HexUtils.getString(hash).toUpperCase();
+//        expect(hashString, equals(expectedHash));
+////      }
+//    });
+  });
+
+  group("Test hasher (SHA3 512)", () {
+    // Test vectors are taken from:
+    // https://github.com/gvanas/KeccakCodePackage/blob/master/TestVectors/ShortMsgKAT_SHA3-512.txt
+    const List<String> expectedOutput = [
+      'A69F73CCA23A9AC5C8B567DC185A756E97C982164FE25859E0D1DCC1475C80A615B2123AF1F5F94C11E3E9402C3AC558F500199D95B6D3E301758586281DCD26',
+      '3939FCC8B57B63612542DA31A834E5DCC36E2EE0F652AC72E02624FA2E5ADEECC7DD6BB3580224B4D6138706FC6E80597B528051230B00621CC2B22999EAA205',
+      'AA092865A40694D91754DBC767B5202C546E226877147A95CB8B4C8F8709FE8CD6905256B089DA37896EA5CA19D2CD9AB94C7192FC39F7CD4D598975A3013C69',
+      'CB20DCF54955F8091111688BECCEF48C1A2F0D0608C3A575163751F002DB30F40F2F671834B22D208591CFAF1F5ECFE43C49863A53B3225BDFD7C6591BA7658B',
+      'D4B4BDFEF56B821D36F4F70AB0D231B8D0C9134638FD54C46309D14FADA92A2840186EED5415AD7CF3969BDFBF2DAF8CCA76ABFE549BE6578C6F4143617A4F1A',
+      'B087C90421AEBF87911647DE9D465CBDA166B672EC47CCD4054A7135A1EF885E7903B52C3F2C3FE722B1C169297A91B82428956A02C631A2240F12162C7BC726'
+    ];
+
+    final int length = 64; // SHA3-512
+
+    test('SHA3 512 can hash test vectors', () {
+      // sanity check
+      expect(expectedOutput.length, equals(inputs.length));
+
+      for (int i = 0; i < inputs.length; i++) {
+        final String inputHex = inputs[i];
+        final Uint8List inputBuffer = HexUtils.getBytes(inputHex);
+        final expectedHash = expectedOutput[i];
+
+        Uint8List hash = Sha3Hasher.hash(inputBuffer, length: length);
+
+        final String hashString = HexUtils.getString(hash).toUpperCase();
+        expect(hashString, equals(expectedHash));
+      }
+    });
   });
 }
