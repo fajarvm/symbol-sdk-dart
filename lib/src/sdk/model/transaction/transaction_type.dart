c6588472a545334ed143c78ebc7cf861449d9727
Fajar van Megen <3041538+fajarvm@users.noreply.github.com>
2019-02-27 14:49:21 +0100

    Added more transaction types as described in the nemtech docs.

--- lib/src/sdk/model/transaction/transaction_type.dart
+++ lib/src/sdk/model/transaction/transaction_type.dart
@@ -16,40 +16,83 @@
 
 library nem2_sdk_dart.sdk.model.transaction.transaction_type;
 
-/// The Transaction type.
+/// The available transaction types.
 class TransactionType {
-  /// The aggregate bonded transaction type.
-  static const int AGGREGATE_BONDED = 0x4241;
-
-  /// The aggregate complete transaction type.
-  static const int AGGREGATE_COMPLETE = 0x4141;
-
-  /// The mosaic definition transaction type.
+  //
+  // Mosaic
+  //
+  /// Register a new mosaic.
   static const int MOSAIC_DEFINITION = 0x414d;
 
-  /// The mosaic levy change transaction type.
-  static const int MOSAIC_LEVY_CHANGE = 0x434d;
-
-  /// The mosaic supply change transaction type.
+  /// Change an existent mosaic supply.
   static const int MOSAIC_SUPPLY_CHANGE = 0x424d;
 
-  /// The modify multi-signature account transaction type.
-  static const int MODIFY_MULTISIG_ACCOUNT = 0x4155;
+  /// Change the levy of a mosaic.
+  static const int MOSAIC_LEVY_CHANGE = 0x434d;
 
-  /// The register namespace transaction type.
-  static const int REGISTER_NAMESPACE = 0x414e;
+  //
+  // Namespace
+  //
+  /// Register a namespace.
+  static const int NAMESPACE_REGISTRATION = 0x414e;
 
-  /// The transfer transaction type.
+  /// Attach a namespace name to an account.
+  static const int NAMESPACE_ATTACH_TO_ACCOUNT = 0x424e;
+
+  /// Attach a namespace name to a mosaic.
+  static const int NAMESPACE_ATTACH_TO_MOSAIC = 0x434e;
+
+  //
+  // Transfer
+  //
+  /// Transfer mosaics and messages between two accounts.
   static const int TRANSFER = 0x4154;
 
-  /// The lock fund transaction type.
-  static const int LOCK_FUND = 0x414C;
+  //
+  // Multi-signature
+  //
+  /// Create or modify a multi-signature contract.
+  static const int MULTISIG_MODIFY = 0x4155;
 
+  /// Send transactions in batches to different accounts.
+  ///
+  /// An aggregate transaction is complete when all the required participants have signed it.
+  static const int MULTISIG_AGGREGATE_COMPLETE = 0x4141;
+
+  /// Propose many transactions between different accounts.
+  ///
+  /// An aggregate transaction is bonded when it requires signatures from other participants.
+  static const int MULTISIG_AGGREGATE_BONDED = 0x4241;
+
+  /// As deposit before announcing aggregate bonded transactions.
+  static const int MULTISIG_HASH_LOCK = 0x4148;
+
+  //
+  // Account filters
+  //
+  /// Allow or block incoming transactions for a given a set of addresses.
+  static const int ACCOUNT_FILTER_ADDRESS = 0x4150;
+
+  /// Allow or block incoming transactions containing a given set of mosaics.
+  static const int ACCOUNT_FILTER_MOSAIC = 0x4250;
+
+  /// Allow or block incoming transactions by transaction type.
+  static const int ACCOUNT_FILTER_ENTITY_TYPE = 0x4350;
+
+  //
+  // Cross-chain swaps
+  //
   /// The secret lock transaction type.
-  static const int SECRET_LOCK = 0x424C;
+  static const int SECRET_LOCK = 0x424C; // TODO: new id is 0x4152 according to nemtech
 
   /// The secret proof transaction type.
-  static const int SECRET_PROOF = 0x434C;
+  static const int SECRET_PROOF = 0x434C; // TODO: new id is 0x4252 according to nemtech
+
+  //
+  // Account link / Remote harvesting
+  //
+  /// Delegates the account importance to a proxy account to enable delegated harvesting.
+  static const int LOCK_FUND = 0x414C;
 
   static final TransactionType singleton = new TransactionType._();
 
@@ -61,22 +104,34 @@ class TransactionType {
 
   static int getTransactionType(final int transactionType) {
     switch (transactionType) {
-      case AGGREGATE_BONDED:
-        return TransactionType.AGGREGATE_BONDED;
-      case AGGREGATE_COMPLETE:
-        return TransactionType.AGGREGATE_COMPLETE;
+      case ACCOUNT_FILTER_ADDRESS:
+        return TransactionType.ACCOUNT_FILTER_ADDRESS;
+      case ACCOUNT_FILTER_ENTITY_TYPE:
+        return TransactionType.ACCOUNT_FILTER_ENTITY_TYPE;
+      case ACCOUNT_FILTER_MOSAIC:
+        return TransactionType.ACCOUNT_FILTER_MOSAIC;
       case MOSAIC_DEFINITION:
         return TransactionType.MOSAIC_DEFINITION;
       case MOSAIC_LEVY_CHANGE:
         return TransactionType.MOSAIC_LEVY_CHANGE;
       case MOSAIC_SUPPLY_CHANGE:
         return TransactionType.MOSAIC_SUPPLY_CHANGE;
-      case MODIFY_MULTISIG_ACCOUNT:
-        return TransactionType.MODIFY_MULTISIG_ACCOUNT;
-      case REGISTER_NAMESPACE:
-        return TransactionType.REGISTER_NAMESPACE;
+      case NAMESPACE_REGISTRATION:
+        return TransactionType.NAMESPACE_REGISTRATION;
+      case NAMESPACE_ATTACH_TO_ACCOUNT:
+        return TransactionType.NAMESPACE_ATTACH_TO_ACCOUNT;
+      case NAMESPACE_ATTACH_TO_MOSAIC:
+        return TransactionType.NAMESPACE_ATTACH_TO_MOSAIC;
       case TRANSFER:
         return TransactionType.TRANSFER;
+      case MULTISIG_MODIFY:
+        return TransactionType.MULTISIG_MODIFY;
+      case MULTISIG_AGGREGATE_BONDED:
+        return TransactionType.MULTISIG_AGGREGATE_BONDED;
+      case MULTISIG_AGGREGATE_COMPLETE:
+        return TransactionType.MULTISIG_AGGREGATE_COMPLETE;
+      case MULTISIG_HASH_LOCK:
+        return TransactionType.MULTISIG_HASH_LOCK;
       case LOCK_FUND:
         return TransactionType.LOCK_FUND;
       case SECRET_LOCK:
--- test/sdk/model/transaction/transaction_type_test.dart
+++ test/sdk/model/transaction/transaction_type_test.dart
@@ -23,31 +23,73 @@ import 'package:nem2_sdk_dart/sdk.dart' show TransactionType;
 void main() {
   group('TransactionType', () {
     test('valid transaction types', () {
-      expect(TransactionType.AGGREGATE_BONDED, 0x4241);
-      expect(TransactionType.AGGREGATE_COMPLETE, 0x4141);
+      // Account filters
+      expect(TransactionType.ACCOUNT_FILTER_ADDRESS, 0x4150);
+      expect(TransactionType.ACCOUNT_FILTER_MOSAIC, 0x4250);
+      expect(TransactionType.ACCOUNT_FILTER_ENTITY_TYPE, 0x4350);
+
+      // Mosaic
       expect(TransactionType.MOSAIC_DEFINITION, 0x414d);
-      expect(TransactionType.MOSAIC_LEVY_CHANGE, 0x434d);
       expect(TransactionType.MOSAIC_SUPPLY_CHANGE, 0x424d);
-      expect(TransactionType.MODIFY_MULTISIG_ACCOUNT, 0x4155);
-      expect(TransactionType.REGISTER_NAMESPACE, 0x414e);
+      expect(TransactionType.MOSAIC_LEVY_CHANGE, 0x434d);
+
+      // Namespace
+      expect(TransactionType.NAMESPACE_REGISTRATION, 0x414e);
+      expect(TransactionType.NAMESPACE_ATTACH_TO_ACCOUNT, 0x424e);
+      expect(TransactionType.NAMESPACE_ATTACH_TO_MOSAIC, 0x434e);
+
+      // Transfer
       expect(TransactionType.TRANSFER, 0x4154);
-      expect(TransactionType.LOCK_FUND, 0x414C);
+
+      // Multi-signature
+      expect(TransactionType.MULTISIG_MODIFY, 0x4155);
+      expect(TransactionType.MULTISIG_AGGREGATE_COMPLETE, 0x4141);
+      expect(TransactionType.MULTISIG_AGGREGATE_BONDED, 0x4241);
+      expect(TransactionType.MULTISIG_HASH_LOCK, 0x4148);
+
+      // Cross-chain swaps
       expect(TransactionType.SECRET_LOCK, 0x424C);
       expect(TransactionType.SECRET_PROOF, 0x434C);
+
+      // Account link / remote harvesting
+      expect(TransactionType.LOCK_FUND, 0x414C);
     });
 
     test('Can retrieve a valid transaction types', () {
-      expect(TransactionType.getTransactionType(0x4241), TransactionType.AGGREGATE_BONDED);
-      expect(TransactionType.getTransactionType(0x4141), TransactionType.AGGREGATE_COMPLETE);
+      // Account filters
+      expect(TransactionType.getTransactionType(0x4150), TransactionType.ACCOUNT_FILTER_ADDRESS);
+      expect(TransactionType.getTransactionType(0x4250), TransactionType.ACCOUNT_FILTER_MOSAIC);
+      expect(
+          TransactionType.getTransactionType(0x4350), TransactionType.ACCOUNT_FILTER_ENTITY_TYPE);
+
+      // Mosaic
       expect(TransactionType.getTransactionType(0x414d), TransactionType.MOSAIC_DEFINITION);
-      expect(TransactionType.getTransactionType(0x434d), TransactionType.MOSAIC_LEVY_CHANGE);
       expect(TransactionType.getTransactionType(0x424d), TransactionType.MOSAIC_SUPPLY_CHANGE);
-      expect(TransactionType.getTransactionType(0x4155), TransactionType.MODIFY_MULTISIG_ACCOUNT);
-      expect(TransactionType.getTransactionType(0x414e), TransactionType.REGISTER_NAMESPACE);
+      expect(TransactionType.getTransactionType(0x434d), TransactionType.MOSAIC_LEVY_CHANGE);
+
+      // Namespace
+      expect(TransactionType.getTransactionType(0x414e), TransactionType.NAMESPACE_REGISTRATION);
+      expect(
+          TransactionType.getTransactionType(0x424e), TransactionType.NAMESPACE_ATTACH_TO_ACCOUNT);
+      expect(
+          TransactionType.getTransactionType(0x434e), TransactionType.NAMESPACE_ATTACH_TO_MOSAIC);
+
+      // Transfer
       expect(TransactionType.getTransactionType(0x4154), TransactionType.TRANSFER);
-      expect(TransactionType.getTransactionType(0x414C), TransactionType.LOCK_FUND);
+
+      // Multi-signature
+      expect(TransactionType.getTransactionType(0x4155), TransactionType.MULTISIG_MODIFY);
+      expect(
+          TransactionType.getTransactionType(0x4141), TransactionType.MULTISIG_AGGREGATE_COMPLETE);
+      expect(TransactionType.getTransactionType(0x4241), TransactionType.MULTISIG_AGGREGATE_BONDED);
+      expect(TransactionType.getTransactionType(0x4148), TransactionType.MULTISIG_HASH_LOCK);
+
+      // Cross-chain swaps
       expect(TransactionType.getTransactionType(0x424C), TransactionType.SECRET_LOCK);
       expect(TransactionType.getTransactionType(0x434C), TransactionType.SECRET_PROOF);
+
+      // Account link / remote harvesting
+      expect(TransactionType.getTransactionType(0x414C), TransactionType.LOCK_FUND);
     });
 
     test('Trying to retrieve an invalid transaction type will throw an error', () {
