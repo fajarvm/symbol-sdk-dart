a3890b7f9b98c38fb19fb815810b055bb409a34e
Fajar van Megen <3041538+fajarvm@users.noreply.github.com>
2019-03-02 15:00:41 +0100

    Exclude dartmft (we are using 100 chars width)

--- tool/travis.sh
+++ tool/travis.sh
@@ -14,13 +14,6 @@ pub get
 echo "Running dartanalyzer..."
 dartanalyzer $DARTANALYZER_FLAGS .
 
-# Verify that dartfmt has been run.
-echo "Checking dartfmt..."
-if [[ $(dartfmt -n --set-exit-if-changed lib/ test/) ]]; then
-  echo "Failed dartfmt check: run dartfmt -w lib/ test/"
-  exit 1
-fi
-
 # Run the tests.
 echo "Running tests..."
 pub run test --reporter expanded
